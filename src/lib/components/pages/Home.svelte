4ed5cc9b3a9df9cd3299a2ad0b5a2cfe5b953917
Wilson Hobbs <4442130+wbhob@users.noreply.github.com>
2025-05-29 00:10:57 -0700

    first commit

---
+++ src/lib/components/pages/Home.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+</script>
+
+<main class="prose">
+	<div class=" container p-4">
+		<h1>wilson hobbs</h1>
+		<p>
+			hi, i'm wilson. i'm a software engineer and designer. i'm working on ai, platform, and design
+			at
+			<a href="https://www.rivet.tax">rivet</a> (acq'd my last co, lobby).
+		</p>
+	</div>
+
+	<div class="container p-4">
+		<div class="flex items-baseline justify-between">
+			<h2>
+				<a href="https://www.rivet.tax" target="_new">Rivet</a>
+			</h2>
+			<p class="text-sm text-gray-500">2024 –</p>
+		</div>
+
+		<p>
+			Rivet is a tech-enabled tax firm that serves some of Silicon Valley's fastest growing
+			companies and investors. Rivet runs on a proprietary platform that makes the client experience
+			seamless and preparer experience efficient. In 2024, Rivet acquired Lobby and I joined as
+			founding engineer.
+		</p>
+	</div>
+
+	<div class="container p-4">
+		<div class="flex items-baseline justify-between">
+			<h2>
+				<a href="https://www.lobby.so" target="_new">Lobby</a>
+			</h2>
+			<p class="text-sm text-gray-500">2021 – 2024</p>
+		</div>
+
+		<p>
+			Lobby focused on making simple and practical applications of emerging technologies, first
+			Decentralized Autonomous Organizations (DAOs) and later generative AI. Our last product, Lobby
+			Studio, was an online workspace for creating AI agents to share with your team, which Rivet
+			acquired in 2024.
+		</p>
+	</div>
+
+	<div class="container p-4">
+		<h2>My Work</h2>
+
+		<p>Before Lobby, I've worked with a handful of startups and funds on creative projects.</p>
+
+		<ul>
+			<li>
+				<a target="_new" href="https://brutalist.vc">Brutalist Ventures</a>, 2021 – Website design
+				<a target="_new" href="https://brutalist.vc">view site</a>
+			</li>
+			<li>
+				<a target="_new" href="https://lolli.com">Lolli</a>, 2020 – Animated Promo Video
+				<a href="https://www.youtube.com/watch?v=vbfSHygQ4uE" target="_new">watch now</a>
+				(has appeared on
+				<a
+					href="https://www.cnn.com/videos/business/2021/04/27/lolli-bitcoin-rewards-crypto.cnnbusiness"
+					target="_new"
+				>
+					CNN
+				</a>)
+			</li>
+
+			<li>
+				Zoomer Company, 2020 – Logo reveal video
+				<a href="https://www.youtube.com/watch?v=bE9aU0BRJiM" target="_new"> watch now </a>
+			</li>
+		</ul>
+	</div>
+	<div class="container p-4">
+		<h2>Investments</h2>
+
+		<p>I angel invest small checks in my friends' companies. Check them out!</p>
+
+		<ul>
+			<li>
+				<a target="_new" href="https://guild.xyz">Guild</a> – Access control protocol for Web 3
+			</li>
+			<li>
+				<a target="_new" href="https://hallparty.app">Hallparty</a> – Smart cards for college campuses
+			</li>
+			<li>
+				<a target="_new" href="https://www.arterial.design">Arterial</a> – Simulation for the future
+				of cities
+			</li>
+			<li>
+				<a target="_new" href="https://withpipeline.com">Pipeline</a> – AI Linkedin outreach for startups
+				& sales teams
+			</li>
+			<li>
+				<a target="_new" href="https://vanoma.com">Vanoma</a> – One-click industry research for beauty
+				brands
+			</li>
+		</ul>
+	</div>
+
+	<div class="container p-4">
+		<h2>Contact</h2>
+		<p>Wilson Hobbs lives in San Francisco, CA.</p>
+		<p>
+			<a href="mailto:wilson@wilsonhobbs.com">wilson@wilsonhobbs.com</a> |
+			<a href="https://x.com/wilsonsaccount">x</a> |
+			<a href="https://github.com/wbhob">github</a> |
+			<a href="https://www.linkedin.com/in/wbhob/">linkedin</a>
+		</p>
+	</div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	import Home from '$lib/components/pages/Home.svelte';
+</script>
+
+<svelte:head>
+	<title>wilson hobbs | home</title>
+</svelte:head>
+<Home />
