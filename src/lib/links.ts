17ba961ed62f37a78c7b764ddc2f920a48fda76d
Wilson Hobbs <4442130+wbhob@users.noreply.github.com>
2026-06-14 00:22:06 -0400

    Update links.ts

--- src/lib/links.ts
+++ src/lib/links.ts
@@ -10,7 +10,7 @@ export interface Link {
 export const links: Link[] = [
 	{
 		title: 'What happens if you click the first link on every Wikipedia article?',
-		url: 'https://www.youtube.com/watch?v=dpLG3DpfSlM',
+		url: 'https://www.youtube.com/watch?v=dpLG3DpfSlM&utm_source=wilsonhobbs.com&utm_medium=referral&utm_campaign=links',
 		date: '13 Jun 2026',
 		type: 'video'
 	}
